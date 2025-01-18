401c6b7b50fa3c37ba20d3fee3f86845a8f159bf
Samuel Sheehy <samuelsheehy95@gmail.com>
2025-01-18 17:26:37 +0000

    Add ruff lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -6,9 +6,10 @@ return {
 		dependencies = {},
 		config = function()
 			local lspconfig = require('lspconfig')
-			
+
 			-- Python
 			lspconfig.pyright.setup({})
+			lspconfig.ruff.setup({})
 
 			-- Key mappings
 			vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = "Go to declaration" })
@@ -20,13 +21,29 @@ return {
 
 			-- Diagnostic settings
 			vim.diagnostic.config({
-				-- set virtual_text to false to not interfere with 
+				-- set virtual_text to false to not interfere with
 				-- rachertier/tiny-inline-diagnostics.nvim
 				virtual_text = false,
 				signs = true,
 				underline = true,
 				update_in_insert = false,
 				severity_sort = true,
+			})
+
+			-- Disable ruff in favour of pyright in some instances
+			vim.api.nvim_create_autocmd("LspAttach", {
+  				 group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
+				callback = function(args)
+    					local client = vim.lsp.get_client_by_id(args.data.client_id)
+    					if client == nil then
+      						return
+    					end
+    					if client.name == 'ruff' then
+      						-- Disable hover in favor of Pyright
+      						client.server_capabilities.hoverProvider = false
+    					end
+  				end,
+  				desc = 'LSP: Disable hover capability from Ruff',
 			})
 		end
 	}
