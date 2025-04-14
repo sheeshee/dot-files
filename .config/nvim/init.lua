8a22e7ade9a576ad78a04b4c00ccf7a4baefe9a3
Samuel Sheehy <samuel.sheehy@breedr.co>
2025-04-14 10:48:16 +0100

    Add nvim-tree

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,10 @@
+-- disable netrw in favour of nvim-tree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true
 -- lazy.vim recommends configuring settings before
 -- loading lazy.vim
 require("sheehy")
 require("config.lazy")
+
 
--- .config/nvim/lua/sheehy/remap.lua
+++ .config/nvim/lua/sheehy/remap.lua
@@ -6,7 +6,5 @@ vim.g.maplocalleader = "\\"
 
 toggle_number = require('sheehy.linenumbers')
 vim.keymap.set('n', '<leader>n', toggle_number, { desc = 'Toggle line number display mode' })
-
-vim.keymap.set('n', '<leader>e', ':Ex<CR>', { desc = 'Open file explorer' })
-
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { desc = 'Open Explorer' })
 vim.keymap.set('n', '<leader>ff', ':FzfLua files<CR>', { desc = 'Fuzzy find files' })
