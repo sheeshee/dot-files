75a72f3b82169cada2464c0a94ecc38f5ae6b59a
Samuel Sheehy <samuelsheehy95@gmail.com>
2025-04-18 22:54:34 +0100

    Add octo.nvim plugin

---
+++ .config/nvim/lua/plugins/github.lua
@@ -0,0 +1,13 @@
+return {
+  'pwntester/octo.nvim',
+  version = "*",
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'ibhagwan/fzf-lua',
+    'nvim-tree/nvim-web-devicons',
+  },
+  cmd = "Octo",
+  opts = {
+    picker = "fzf-lua"
+  },
+}
