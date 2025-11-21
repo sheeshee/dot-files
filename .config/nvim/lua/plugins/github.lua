ff72de5e6f1f4909cf879ed313791b7c551bd9df
Samuel Sheehy <samuel.sheehy@breedr.co>
2025-11-21 17:19:30 +0000

    [nvim] Add keymap to open PRs awaiting review

--- .config/nvim/lua/plugins/github.lua
+++ .config/nvim/lua/plugins/github.lua
@@ -8,14 +8,21 @@ return {
   },
   cmd = "Octo",
   opts = {
-    picker = "fzf-lua",
+    -- use snacks instead of fzf-lua while there is an
+    -- error with https://github.com/pwntester/octo.nvim/issues/1147
+    picker = "snacks",
     use_local_fs = true
   },
   keys = {
     {
       "<leader>gP",
       "<CMD>Octo pr list<CR>",
-      desc = "List GitHub Pull Requests",
+      desc = "List All PRs",
+    },
+    {
+      "<leader>gp",
+      "<CMD>Octo search is:pr is:open user-review-requested:@me<CR>",
+      desc = "List PRs Awaiting Review"
     }
   }
 }
