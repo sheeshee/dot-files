69622379dd2ae0b33307f9fd63acb61ebbaa3cdf
Samuel Sheehy <samuel.sheehy@breedr.co>
2025-11-21 09:45:17 +0000

    [nvim] replace tabs with spaces

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,57 +1,57 @@
 return {
-	-- Core LSP support
-	{
-		"neovim/nvim-lspconfig",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {},
-		config = function()
-			local lspconfig = vim.lsp.config
-			-- Python
-			lspconfig("pyright", {})
-			lspconfig("ruff", {})
+  -- Core LSP support
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {},
+    config = function()
+      local lspconfig = vim.lsp.config
+      -- Python
+      lspconfig("pyright", {})
+      lspconfig("ruff", {})
 
 			vim.lsp.enable({"ruff", "pyright"})
 
 
-			-- Key mappings
-			vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = "Go to declaration" })
-      			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "Go to definition" })
-      			vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = "Show hover information" })
-			vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { desc = "Go to implementation" })
-			vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = "Rename symbol" })
-			vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = "Code actions" })
+      -- Key mappings
+      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = "Go to declaration" })
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = "Go to definition" })
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = "Show hover information" })
+      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { desc = "Go to implementation" })
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = "Rename symbol" })
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = "Code actions" })
 
-			-- Diagnostic settings
-			vim.diagnostic.config({
-				-- set virtual_text to false to not interfere with
-				-- rachertier/tiny-inline-diagnostics.nvim
-				virtual_text = false,
-				signs = true,
-				underline = true,
-				update_in_insert = false,
-				severity_sort = true,
-			})
+      -- Diagnostic settings
+      vim.diagnostic.config({
+        -- set virtual_text to false to not interfere with
+        -- rachertier/tiny-inline-diagnostics.nvim
+        virtual_text = false,
+        signs = true,
+        underline = true,
+        update_in_insert = false,
+        severity_sort = true,
+      })
 
-			-- Disable ruff in favour of pyright in some instances
-			vim.api.nvim_create_autocmd("LspAttach", {
-  				 group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
-				callback = function(args)
-    					local client = vim.lsp.get_client_by_id(args.data.client_id)
-    					if client == nil then
-      						return
-    					end
-    					if client.name == 'ruff' then
-      						-- Disable hover in favor of Pyright
-      						client.server_capabilities.hoverProvider = false
-    					end
-  				end,
-  				desc = 'LSP: Disable hover capability from Ruff',
-			})
-		end
-	},
-	{
-		"pmizio/typescript-tools.nvim",
-		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-		opts = {},
-	}
+      -- Disable ruff in favour of pyright in some instances
+      vim.api.nvim_create_autocmd("LspAttach", {
+           group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
+        callback = function(args)
+              local client = vim.lsp.get_client_by_id(args.data.client_id)
+              if client == nil then
+                  return
+              end
+              if client.name == 'ruff' then
+                  -- Disable hover in favor of Pyright
+                  client.server_capabilities.hoverProvider = false
+              end
+          end,
+          desc = 'LSP: Disable hover capability from Ruff',
+      })
+    end
+  },
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {},
+  }
 }
