61ed98cb49040670b05c1982775ca26271eb0e63
Samuel Sheehy <samuel.sheehy@breedr.co>
2025-11-21 17:20:10 +0000

    [nvim] Add keymaps from the primeagen

--- .config/nvim/lua/sheehy/remap.lua
+++ .config/nvim/lua/sheehy/remap.lua
@@ -4,14 +4,19 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-toggle_number = require('sheehy.linenumbers')
-vim.keymap.set('n', '<leader>n', toggle_number, { desc = 'Toggle line number display mode' })
-vim.keymap.set('n', '<leader>w', ':set wrap!<CR>', { desc = 'Toggle line wrap' })
-vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { desc = 'Open Explorer' })
-vim.keymap.set('n', '<leader>ff', ':FzfLua files<CR>', { desc = 'Fuzzy find files' })
-vim.keymap.set('n', '<leader>fg', ':FzfLua grep<CR>', { desc = 'Grep files' })
-vim.keymap.set('n', '<leader>fo', ':FzfLua oldfiles<CR>', { desc = 'View oldfiles' })
-vim.keymap.set('n', '<leader>fr', ':FzfLua resume<CR>', { desc = 'Open last finder window' })
-vim.keymap.set('n', '<leader>fs', ':FzfLua lsp_document_symbols<CR>', { desc = 'View document symbols' })
-vim.keymap.set('n', '<leader>fb', ':FzfLua buffers<CR>', { desc = 'View buffers' })
+toggle_number = require("sheehy.linenumbers")
+vim.keymap.set("n", "<leader>n", toggle_number, { desc = "Toggle line number display mode" })
+vim.keymap.set("n", "<leader>w", ":set wrap!<CR>", { desc = "Toggle line wrap" })
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Open Explorer" })
+vim.keymap.set("n", "<leader>ff", ":FzfLua files<CR>", { desc = "Fuzzy find files" })
+vim.keymap.set("n", "<leader>fg", ":FzfLua grep<CR>", { desc = "Grep files" })
+vim.keymap.set("n", "<leader>fo", ":FzfLua oldfiles<CR>", { desc = "View oldfiles" })
+vim.keymap.set("n", "<leader>fr", ":FzfLua resume<CR>", { desc = "Open last finder window" })
+vim.keymap.set("n", "<leader>fs", ":FzfLua lsp_document_symbols<CR>", { desc = "View document symbols" })
+vim.keymap.set("n", "<leader>fb", ":FzfLua buffers<CR>", { desc = "View buffers" })
 
+-- From the Primeagen https://www.youtube.com/watch?v=w7i4amO_zaE
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")  -- move selection up/down
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "n", "nzzzv")  -- keep cursor centered when cycling through searches
