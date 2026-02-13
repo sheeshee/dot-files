4d460a09bcee6184a8e70baa542361b9ae82b064
Samuel Sheehy <samuelsheehy95@gmail.com>
2026-02-13 18:21:58 +0000

    [nvim] add bufferline.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,7 @@
 -- disable netrw in favour of nvim-tree
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+-- termguicolors strongly recommended for bufferline.nvim
 vim.opt.termguicolors = true
 -- lazy.vim recommends configuring settings before
 -- loading lazy.vim
