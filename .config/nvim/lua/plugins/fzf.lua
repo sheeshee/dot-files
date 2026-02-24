b7e47ea91a6074538b295294a761d3e498953fd4
Samuel Sheehy <samuel.sheehy@breedr.co>
2026-02-24 09:52:03 +0000

    [nvim] Move (and update) FzfLua keymaps

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -4,5 +4,19 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   -- or if using mini.icons/mini.nvim
   -- dependencies = { "echasnovski/mini.icons" },
-  opts = {}
+  opts = {
+
+  },
+  config = function(_, opts)
+    require("fzf-lua").setup(opts)
+    vim.keymap.set("n", "<leader>ff", ":FzfLua files<CR>", { desc = "Fuzzy find files" })
+    vim.keymap.set("n", "<leader>fg", ":FzfLua grep<CR>", { desc = "Grep files" })
+    vim.keymap.set("n", "<leader>fh", ":FzfLua history<CR>", { desc = "View history" })
+    vim.keymap.set("n", "<leader>fc", ":FzfLua resume<CR>", { desc = "Open last finder window" })
+    vim.keymap.set("n", "<leader>fs", ":FzfLua lsp_document_symbols<CR>", { desc = "View document symbols" })
+    vim.keymap.set("n", "<leader>fb", ":FzfLua buffers<CR>", { desc = "View buffers" })
+    vim.keymap.set("n", "<leader>fr", ":FzfLua lsp_references<CR>", { desc = "View references" })
+    vim.keymap.set("n", "<leader>fj", ":FzfLua jumps<CR>", { desc = "View jumps" })
+    vim.keymap.set("n", "<leader>f*", ":FzfLua grep_cword<CR>", { desc = "Grep word under cursor" })
+  end,
 }
--- .config/nvim/lua/sheehy/remap.lua
+++ .config/nvim/lua/sheehy/remap.lua
@@ -8,14 +8,6 @@ local toggle_number = require("sheehy.linenumbers")
 vim.keymap.set("n", "<leader>n", toggle_number, { desc = "Toggle line number display mode" })
 vim.keymap.set("n", "<leader>w", ":set wrap!<CR>", { desc = "Toggle line wrap" })
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Open Explorer" })
-vim.keymap.set("n", "<leader>ff", ":FzfLua files<CR>", { desc = "Fuzzy find files" })
-vim.keymap.set("n", "<leader>fg", ":FzfLua grep<CR>", { desc = "Grep files" })
-vim.keymap.set("n", "<leader>fo", ":FzfLua oldfiles<CR>", { desc = "View oldfiles" })
-vim.keymap.set("n", "<leader>fc", ":FzfLua resume<CR>", { desc = "Open last finder window" })
-vim.keymap.set("n", "<leader>fs", ":FzfLua lsp_document_symbols<CR>", { desc = "View document symbols" })
-vim.keymap.set("n", "<leader>fb", ":FzfLua buffers<CR>", { desc = "View buffers" })
-vim.keymap.set("n", "<leader>fr", ":FzfLua lsp_references<CR>", { desc = "View references" })
-vim.keymap.set("n", "<leader>fj", ":FzfLua jumps<CR>", { desc = "View jumps" })
 
 -- From the Primeagen https://www.youtube.com/watch?v=w7i4amO_zaE
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")  -- move selection up/down
