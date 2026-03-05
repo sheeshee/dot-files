d935fd867d9b3110607ac7ace47d2f02f9eb6a3f
Samuel Sheehy <samuel.sheehy@breedr.co>
2026-03-05 11:21:02 +0000

    [nvim] add autosave

--- .config/nvim/lua/sheehy/editor.lua
+++ .config/nvim/lua/sheehy/editor.lua
@@ -1,2 +1,8 @@
 -- how many lines to leave above/below cursor when scrolling through file
 vim.opt.scrolloff = 3
+
+-- save buffer when focus is lost or switching buffers
+vim.api.nvim_create_autocmd({ "FocusLost", "BufLeave" }, {
+  pattern = "*",
+  command = "silent! wa",
+})
